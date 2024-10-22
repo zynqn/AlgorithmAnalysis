b33350ae92cb299536410dc49a8463a36bc64c6d
zynqn <37334895+zynqn@users.noreply.github.com>
2024-10-22 18:53:21 +0800

    initial update 2
    1. linking of opencv
    2. initial code and image window
    3. post build events

--- AlgorithmAnal/AlgorithmAnal Ass 2 Q3/AlgorithmAnal Ass 2 Q3.cpp
+++ AlgorithmAnal/AlgorithmAnal Ass 2 Q3/AlgorithmAnal Ass 2 Q3.cpp
@@ -1,20 +1,21 @@
-// AlgorithmAnal Ass 2 Q3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// each project has their own asset folder, make sure you place the image you want inside the respective asset folders and it should copy over. 
+	cv::Mat img = cv::imread("assets/test3.png");
+
+	// the name of the window has to be the same, test open CV for all 3 etc
+	namedWindow("Test open CV 3", WINDOW_AUTOSIZE);
+	cv::imshow("Test open CV 3", img);
+	cv::moveWindow("Test open CV 3", 0, 45);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
