7c23debaeefe961386597e85122a5ef0894ca208
wagyulover69 <georgekoh123@gmail.com>
2024-11-19 16:12:54 +0800

    fixed redefinition error

--- AlgorithmAnal/AlgorithmAnal Ass 2 Q3/Utility.h
+++ AlgorithmAnal/AlgorithmAnal Ass 2 Q3/Utility.h
@@ -25,7 +25,7 @@ constexpr int THRESHOLD = 8;
 #define WRAP(x)
 #endif
 
-cv::Mat brushMask;  // Declaration hi
+extern cv::Mat brushMask;  // Declaration hi
 extern bool isDrawing;
 extern int brushSize;
 extern bool maskInitialized;
