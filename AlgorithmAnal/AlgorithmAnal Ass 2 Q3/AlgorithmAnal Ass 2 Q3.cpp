bbb1c4c0e033a95494a767e32697419f44994675
zynqn <37334895+zynqn@users.noreply.github.com>
2024-11-22 10:17:26 +0800

    graph

--- AlgorithmAnal/AlgorithmAnal Ass 2 Q3/AlgorithmAnal Ass 2 Q3.cpp
+++ AlgorithmAnal/AlgorithmAnal Ass 2 Q3/AlgorithmAnal Ass 2 Q3.cpp
@@ -15,6 +15,10 @@
 // graph
 #include "matplotlibcpp.h"
 
+#include <functional> // For std::bind
+#include <thread>
+
+
 // maxflow for graph cut
 #include "graph.h"
 
@@ -25,8 +29,91 @@ bool isDrawing = false;
 int brushSize = 10;
 bool maskInitialized = false;
 
+
+
+
+void plotter(cv::Mat imgTar)
+{
+    std::cout << "plotting\n";
+
+    // Split the image into its 3 channels (B, G, R)
+    std::vector<cv::Mat> channels2;
+    cv::split(imgTar, channels2);  // channels[0] = Blue, channels[1] = Green, channels[2] = Red
+
+    // get original energy map
+    cv::Mat energyMapper = CalculateEnergyMap(channels2);
+    cv::Mat displayEnergyMapper;
+
+    // Convert the energy map back to 8-bit format for display
+    cv::normalize(energyMapper, energyMapper, 0, 255, cv::NORM_MINMAX);
+    energyMapper.convertTo(displayEnergyMapper, CV_8U);
+
+    int rows2 = energyMapper.rows, cols2 = energyMapper.cols;
+
+    // ===================== 
+    // PREPARE 3D PLOT 
+    // =====================
+    std::vector<std::vector<double>> X2(rows2, std::vector<double>(cols2));
+    std::vector<std::vector<double>> Y2(rows2, std::vector<double>(cols2));
+    std::vector<std::vector<double>> Z2(rows2, std::vector<double>(cols2));
+
+    for (int i = 0; i < rows2; ++i)
+    {
+        for (int j = 0; j < cols2; ++j)
+        {
+            X2[i][j] = j;  // X-coordinate (columns)
+            Y2[i][j] = i;  // Y-coordinate (rows)
+            Z2[i][j] = displayEnergyMapper.at<uchar>(i, j);  // Z-value (intensity)
+        }
+    }
+
+    //plt::figure();
+    plt::plot_surface(X2, Y2, Z2);  // Plot the 3D surface
+    //plt::save("assets/output.png"); // Saves the plot as a PNG file
+    /*cv::Mat energyMapImg = cv::imread("assets/output.png");
+    cv::imshow("Energy Map Graph", energyMapImg);*/
+
+    plt::show();
+    //plt::pause(1.0);
+
+    
+
+    //plt::clf();
+    //plt::close();
+    //plt::show();  // Display the plot once
+
+}
+
+void plotter2(std::vector<std::vector<double>>& X2, std::vector<std::vector<double>>& Y2, std::vector<std::vector<double>>& Z2)
+{
+    //plt::figure();
+    plt::plot_surface(X2, Y2, Z2);  // Plot the 3D surface
+   
+    plt::show();
+    plt::pause(1.0);
+    
+
+}
+
+
+void multiThreadPlot(cv::Mat imgTarget)
+{
+    std::thread plot_thread(std::bind(plotter, std::ref(imgTarget)));
+    plot_thread.detach(); // Let the thread run independently
+}
+
+void multiThreadPlot2(std::vector<std::vector<double>>& X2, std::vector<std::vector<double>>& Y2, std::vector<std::vector<double>>& Z2)
+{
+    //std::thread plot_thread(std::bind(plotter2, std::ref(X2), std::ref(Y2), std::ref(Z2) ) );
+    std::thread plot_thread(std::bind(plotter2, X2, Y2, Z2));
+    plot_thread.detach(); // Let the thread run independently
+}
+
 int main()
 {
+
+
+
     // ==============
     // LOAD THE IMAGE
     // ==============
@@ -99,8 +186,22 @@ int main()
         }
     }
 
-    plt::plot_surface(X, Y, Z);  // Plot the 3D surface
-    plt::show();  // Display the plot once
+    //plt::plot_surface(X, Y, Z);  // Plot the 3D surface
+    //plt::show();  // Display the plot once
+    //plt::clf();
+    //plt::close();
+
+    //plotter(imgClone);
+
+    //std::thread plot_thread(std::bind(plotter, imgClone));
+
+    //plot_thread.detach(); // Let the thread run independently
+    // 
+    //std::thread plot_thread(plotter);
+
+    multiThreadPlot2(X,Y,Z);
+    //plotter2(X, Y, Z);
+    
 
 #endif
 
@@ -112,7 +213,11 @@ int main()
         int key = cv::waitKey(1);
 
         if (key == 'c')
+        {
             SeamCarvingToWidth(imgClone, 500);
+            //
+        }
+            
 
         if (key == 'b')
             SeamCarvingToHeight(imgClone, 400);
@@ -121,6 +226,9 @@ int main()
         {
             ContentAwareRemoval(imgClone);
             cv::imshow("Original Image", imgClone);
+            //multiThreadPlot(imgClone);
+            //multiThreadPlot2(X, Y, Z);
+            
         }
         else if (key == 'r')
         {
@@ -130,8 +238,11 @@ int main()
         }
         else if (key == cv::ESC_KEY)
             break;
+
+        //multiThreadPlot2(X, Y, Z);
     }
 
     cv::destroyAllWindows();
+    //std::this_thread::sleep_for(std::chrono::seconds(5));
     return 0;
 }
